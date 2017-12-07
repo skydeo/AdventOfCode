e3eff3969339524b3878a558b3ad33de025b22a8
Alexander Stone <enderu@gmail.com>
2017-12-07 10:00:59 -0600

    I don't anticipate this code will be of much use in the second part, but it works!

---
+++ 2017/day07/aoc07a.py
@@ -0,0 +1,37 @@
+import timeit
+sTime = timeit.default_timer()
+
+testing = 0
+
+if testing:
+    filename = "testInput.txt"
+else:
+    filename = "input.txt"
+
+data = []
+with open(filename) as f:
+    i = f.read().splitlines()
+    for j in i:
+        data.append(j.split())
+
+children = []
+words = []
+for r in data:
+    # print(r)
+    if '->' in r:
+        for c in r[r.index('->')+1:]:
+            children.append(c.strip(','))
+        words.append(r[0])
+    else:
+        children.append(r[0])
+
+# print(children)
+# print(words)
+
+for w in words:
+    if w not in children:
+        print("Root program: {0}".format(w))
+
+
+eTime = timeit.default_timer()
+print("Completed in {0} seconds".format(round(eTime - sTime,5)))
