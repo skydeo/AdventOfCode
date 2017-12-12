2d6ed6f96d524c5aba5202dc76574438d7909b4f
Alexander Stone <enderu@gmail.com>
2017-12-12 12:59:39 -0600

    Super easy part 2. Obviously could just store max if max > max if input size was a concern, but never is for these scales.

--- 2017/day11/aoc11.py
+++ 2017/day11/aoc11.py
@@ -15,6 +15,8 @@ else:
     moves = f.read().rstrip().split(",")
 
 c = {'x': 0, 'y': 0, 'z': 0}
+distances = []
+
 for m in moves:
     if m == "ne":
         c['x'] += 1
@@ -36,11 +38,11 @@ for m in moves:
         c['y'] -= 1
     else:
         print("ERROR: {0} is not a valid instruction.".format(m))
+    
+    distances += [int((abs(c['x']) + abs(c['y']) + abs(c['z'])) / 2)]
 
-distance = int((abs(c['x']) + abs(c['y']) + abs(c['z'])) / 2)
-
-print("Distance from origin: {0}".format(distance))
-
+print("Distance from origin: {0}".format(distances[-1]))
+print("Maximum distance from origin: {0}".format(max(distances)))
 
 eTime = timeit.default_timer()
 print("Completed in {0} seconds".format(round(eTime - sTime,5)))
