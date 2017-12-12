1768e7fc026ce0b30a74e029785c45b773fe240b
Alexander Stone <enderu@gmail.com>
2017-12-12 09:56:55 -0600

    Day 7 has gotten messier, but no closer to the answer. I think I'm going to have to start from scratch at some point.

--- 2017/day07/aoc07b.py
+++ 2017/day07/aoc07b.py
@@ -1,7 +1,7 @@
 import timeit
 sTime = timeit.default_timer()
 
-testing = 1
+testing = 0
 
 if testing:
     filename = "testInput.txt"
@@ -18,9 +18,11 @@ children = []
 words = []
 weights = {}
 hierarchy = {}
+diskWeights = {}
+
 
 for r in data:
-    print(r)
+    # print(r)
     weights[r[0]] = int(r[1][1:-1])
     if '->' in r:
         kids = []
@@ -32,6 +34,7 @@ for r in data:
         hierarchy[r[0]] = kids
     else:
         children.append(r[0])
+        diskWeights[r[0]] = r[1][1:-1]
 
 for w in words:
     if w not in children:
@@ -49,29 +52,70 @@ def calculateWeights(rootProgram):
 
 
 
-print(calculateWeights('ugml'))
-print(calculateWeights('padx'))
-print(calculateWeights('fwft'))
-print(calculateWeights(root))
+# print("Weight of ugml: {0}".format(calculateWeights('ugml')))
+# print("Weight of padx: {0}".format(calculateWeights('padx')))
+# print("Weight of fwft: {0}".format(calculateWeights('fwft')))
+# print("Weight of root word ({0}): {1}".format(root, calculateWeights(root)))
 
-diskWeights = {}
 
 for p in hierarchy:
     diskWeights[p] = calculateWeights(p)
 
-print(diskWeights)
+# print("diskWeights: ",diskWeights)
+# print("hierarcy: ",hierarchy)
 
 # I'm so far deep at this point in not building a tree, a foolish decision made thinking it would be
 # unnecessarily laborious. I don't know if I should just give in and make a tree, use a library, or
 # charge needlessly forward.
+# Should've learned https://networkx.github.io
 
-for x in hierarchy:
-    print(x)
+def findImbalance(word):
+    imbalancedWord = ''
+    # print("Working on word: {0}".format(word))
+    if word in hierarchy:
+        tWeights = {}
+        for x in hierarchy[word]:
+            # print(x, diskWeights[x])
+            w = diskWeights[x]
+            if w in tWeights:
+                tWeights[diskWeights[x]] += 1
+            else:
+                tWeights[diskWeights[x]] = 1
+        
+        wrongValue = min(tWeights, key=tWeights.get)
+
+        for x in hierarchy[word]:
+            if diskWeights[x] == wrongValue:
+                imbalancedWord = x
+                findImbalance(x)
+
+    return imbalancedWord
+
+# print(hierarchy)
+
+ibw = findImbalance(root)
+
+for w in hierarchy:
+    if ibw in hierarchy[w]:
+        ibwp = w
 
 
+tWeights = {}
+for x in hierarchy[ibwp]:
+    w = diskWeights[x]
+    if w in tWeights:
+        tWeights[diskWeights[x]] += 1
+    else:
+        tWeights[diskWeights[x]] = 1
+print(tWeights)
+correction = max(tWeights, key=tWeights.get) - min(tWeights, key=tWeights.get)
 
 
+print(correction)
 
+cpv = weights[ibwp] + correction
+
+print(cpv)
 
 
 eTime = timeit.default_timer()
