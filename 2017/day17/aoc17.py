0a85d7d6d2d63dbb5f95fbd70326f804cb741127
Alexander Stone <enderu@gmail.com>
2017-12-19 15:33:40 -0600

    Brute force is fun.

--- 2017/day17/aoc17.py
+++ 2017/day17/aoc17.py
@@ -9,16 +9,19 @@ if testing:
 else:
     steps = 369
 
-iterations = 2017
+p1i = 2_017
+p2i = 50_000_000
 
 def createBuffer(iterations):
     buffer = deque([0])
     for i in range(0,iterations):
         buffer.rotate(-steps)
         buffer.append(i+1)
-    return buffer[0]
+    return buffer
 
-print('Value after last created buffer: {0}'.format(createBuffer(iterations)))
+print('Value after last created buffer on iteration {0}: {1}'.format(p1i, createBuffer(p1i)[0]))
+part2 = createBuffer(p2i)
+print('Value after 0 on iteration {0}: {1}'.format(p2i,part2[part2.index(0)+1]))
 
 eTime = timeit.default_timer()
 print("Completed in {0} seconds".format(round(eTime - sTime,5)))
