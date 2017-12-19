7f8cde81ec22f904e1c50744e59e3ba83b3c0f81
Alexander Stone <enderu@gmail.com>
2017-12-19 14:10:48 -0600

    Clean up

--- 2017/day16/aoc16.py
+++ 2017/day16/aoc16.py
@@ -22,17 +22,13 @@ while True:
             line = line[-v:] + line[:-v]
         elif m == 'x':
             pos1, pos2 = [int(n) for n in i[1:].split('/')]
-            p1 = line[pos1]
-            line[pos1] = line[pos2]
-            line[pos2] = p1
+            line[pos1], line[pos2] = line[pos2], line[pos1]
         elif m == 'p':
             p1, p2 = i[1:].split('/')
-            pos1 = line.index(p1)
-            pos2 = line.index(p2)
-            line[pos1] = p2
-            line[pos2] = p1
+            pos1, pos2 = line.index(p1), line.index(p2)
+            line[pos1], line[pos2] = p2, p1
         else:
-            print('Unrecognized move: {}'.format(i))
+            print('Unrecognized move: {0}'.format(i))
 
     dancedArray = ''.join(line)
 
