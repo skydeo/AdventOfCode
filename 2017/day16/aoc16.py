27f0539a4a7484108544669663d0ca7a4e99ca17
Alexander Stone <enderu@gmail.com>
2017-12-19 11:47:26 -0600

    Not real pretty. Obviously with 1000000000 iterations that's only feasible on certain hardware with certain (almost certainly compiled?) languages, so there had to be another way. I thought maybe there was a pattern in the instructions, but that seemed complicated, so I first checked if there was a pattern in the output, and sure enough there was. So then it was just a matter of finding it and doing some math around that.

--- 2017/day16/aoc16.py
+++ 2017/day16/aoc16.py
@@ -11,26 +11,43 @@ else:
     f = open('16.in','r')
     instructions = f.read().rstrip().split(',')
 
-for i in instructions:
-    m = i[0]
-    if m == 's':
-        v = int(i[1:])
-        line = line[-v:] + line[:-v]
-    elif m == 'x':
-        pos1, pos2 = [int(n) for n in i[1:].split('/')]
-        p1 = line[pos1]
-        line[pos1] = line[pos2]
-        line[pos2] = p1
-    elif m == 'p':
-        p1, p2 = i[1:].split('/')
-        pos1 = line.index(p1)
-        pos2 = line.index(p2)
-        line[pos1] = p2
-        line[pos2] = p1
-    else:
-        print('Unrecognized move: {}'.format(i))
+iterations = 1e9
 
-print(''.join(line))
+finalPositions = {}
+while True:
+    for i in instructions:
+        m = i[0]
+        if m == 's':
+            v = int(i[1:])
+            line = line[-v:] + line[:-v]
+        elif m == 'x':
+            pos1, pos2 = [int(n) for n in i[1:].split('/')]
+            p1 = line[pos1]
+            line[pos1] = line[pos2]
+            line[pos2] = p1
+        elif m == 'p':
+            p1, p2 = i[1:].split('/')
+            pos1 = line.index(p1)
+            pos2 = line.index(p2)
+            line[pos1] = p2
+            line[pos2] = p1
+        else:
+            print('Unrecognized move: {}'.format(i))
+
+    dancedArray = ''.join(line)
+
+    rNum = len(finalPositions)
+    if dancedArray in finalPositions.values():
+        print('Loop size found: {0}'.format(rNum))
+        break;
+    else:
+        finalPositions[rNum] = dancedArray
+
+loopSize = len(finalPositions)
+finalStop = iterations % loopSize
+
+print('Part 1: {0}'.format(finalPositions[0]))
+print('Part 2: {0}'.format(finalPositions[finalStop-1]))
 
 eTime = timeit.default_timer()
 print("Completed in {0} seconds".format(round(eTime - sTime,5)))
