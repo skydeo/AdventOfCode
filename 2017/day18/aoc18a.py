0fc9bcc0017beb9b28ae72eb751fa7728b71ee0d
Alexander Stone <enderu@gmail.com>
2017-12-20 11:23:47 -0600

    Part 2 looks comlicated.

--- 2017/day18/aoc18a.py
+++ 2017/day18/aoc18a.py
@@ -22,7 +22,6 @@ def is_int(s):
 i = 0
 while i in range(0,len(instructions)):
     ins = instructions[i]
-    print('i: {}\tins: {}'.format(i,ins))
     c = ins.split()[0]
 
     if c == 'set':
@@ -30,25 +29,21 @@ while i in range(0,len(instructions)):
         if not is_int(y):
             y = regs[y]
         regs[x] = int(y)
-        print('\tSetting register {} to {}.'.format(x,y))
     elif c == 'add':
         x, y = ins.split()[1:]
         if not is_int(y):
             y = regs[y]
         regs[x] += int(y)
-        print('\tAdding {} to register {}.'.format(y,x))        
     elif c == 'mul':
         x, y = ins.split()[1:]
         if not is_int(y):
             y = regs[y]
         regs[x] *= int(y)
-        print('\tMultiplying register {} by {}.'.format(x,y))   
     elif c =='mod':
         x, y = ins.split()[1:]
         if not is_int(y):
             y = regs[y]
         regs[x] %= int(y)
-        print('\tModulo-ing register {} by {}.'.format(x,y))   
     elif c == 'jgz':
         x, y = ins.split()[1:]
         if not is_int(x):
@@ -57,13 +52,9 @@ while i in range(0,len(instructions)):
             y = regs[y]
         if x > 0:
             i += int(y) - 1
-        else:
-            print('\t{} not greater than 0. Not jumping.'.format(x))
-        print('\tJump\tins:{0}, x: {1}, y: {2}'.format(ins,x,y))
     elif c == 'snd':
         x = ins.split()[1]
         freq = regs[x]
-        print('\tSound played: {0}'.format(freq))
     elif c == 'rcv':
         x = ins.split()[1]
         if regs[x] != 0:
@@ -73,8 +64,6 @@ while i in range(0,len(instructions)):
         print('Unrecognized instruction: {0}'.format(ins))
     
     i += 1
-    # print(i)
-    print('\t',dict(regs))
 
 eTime = timeit.default_timer()
 print("Completed in {0} seconds".format(round(eTime - sTime,5)))
